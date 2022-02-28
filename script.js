5f95fa144da14b0c1f5e50478b2b65a40080c5a0
dr-developer27 <twentys245@gmail.com>
2022-02-28 16:59:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener("scroll", function () {
+  let nav = document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+const menutoggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+menutoggle.addEventListener("click", function () {
+  let nav = document.querySelector("nav");
+  menutoggle.classList.toggle("active");
+  menu.classList.toggle("slide-menu");
+  nav.classList.toggle("sticky");
+});
+
+const list = document.querySelector(".list");
+list.addEventListener("click", function () {
+  menutoggle.classList.remove("active");
+  menu.classList.remove("slide-menu");
+});
+
+
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav .menu");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
